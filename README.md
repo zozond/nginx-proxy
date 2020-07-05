5b8f67c9913d3d9360a449cdc4db0228b1d95207
unknown <zozond@naver.com>
2020-07-05 17:43:53 +0900

    2020-07-05

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # nginx-proxy
+
+# Docker Build 
+docker build -t nginx-proxy .
+
+# Docker Run
+docker run -d -p 3000:3000 -p 10000:10000 --name proxy nginx-proxy
+
+# Usage
+추가: curl -X POST 192.168.99.100:3000/add -H "Content-Type: application/json" -d '{"id":"id", "name": "name", "ip": "172.17.0.3"}' 
+삭제: curl -X POST 192.168.99.100:3000/remove -H "Content-Type: application/json" -d '{"id":"id", "name": "name", "ip": "172.17.0.3"}'
+접속: curl 192.168.99.100:10000/id/name/
+
+# Description
+list.conf : rest-api를 통해 받은 프록시 대상 목록
+default.conf : nginx 프록시 conf파일로 복사 시킬 템플릿
+nginx.conf : nginx 프록시를 위해 필요한 설정을 등록시킬 템플릿 
+handleList.js : list.conf 및 default.conf 파일 핸들링 함수들
+logger.js : 로그 
+index.js : 실제로 동작하는 파일
+
+새로운 Nginx Proxy가 등록되었는지 확인하는데 걸리는 시간 : 10초
--- index.js
+++ index.js
@@ -13,18 +13,6 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json());
 
-/* docker build -t nginx-proxy . */
-/* docker run -d -p 3000:3000 -p 10000:10000 --name proxy nginx-proxy */
-
-/* 
-    #### 내가 가지고 있을 대상 ####
-    list.conf : rest-api를 통해 받은 프록시 대상 목록
-
-    #### 복사 시킬 대상 ####
-    default.conf : nginx 프록시 conf파일로 복사 시킬 템플릿
-    nginx.conf : nginx 프록시를 위해 필요한 설정을 등록시킬 템플릿 
-*/
-
 /* 
     queue에 들어갈 item 
     {
@@ -66,6 +54,7 @@ function doQueue(){
 /* 10 초에 한번 리로드 함 */
 setInterval(doQueue, 10000);
 
+/* Router */
 app.get('/', (req, res) => {
     res.status(200).send('Nginx-Proxy Rest-Api is Running!!! ');
     return;
@@ -74,10 +63,6 @@ app.get('/', (req, res) => {
 app.get('/version', (req, res) => {
     res.status(200).send('Nginx-Proxy Rest-Api v1.0.0');
 })
-
-/* 추가: curl -X POST 192.168.99.100:3000/add -H "Content-Type: application/json" -d '{"id":"1", "name": "name", "ip": "172.17.0.3"}' */
-/* 삭제: curl -X POST 192.168.99.100:3000/remove -H "Content-Type: application/json" -d '{"id":"id", "name": "name", "ip": "172.17.0.3"}' */
-/* 접속: curl 192.168.99.100:10000/id/name/ */
 
 app.post('/add', (req, res) => {
     var ip = req.headers['x-forwarded-for'] ||  req.connection.remoteAddress;
